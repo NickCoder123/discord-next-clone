8764c67ddb428125e45f77cf216778770cf7a438
Nikunj Mehra <nikunjmehra935@gmail.com>
2023-09-18 22:24:55 +0530

    Folder setup

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Login Now!</h1>
+      <Button>
+        Login
+      </Button>
+    </div>
+  )
+}
---
+++ app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function RegisterPage() {
+  return (
+    <div>
+      <h1>Register Now!</h1>
+      <Button>
+        Register
+      </Button>
+    </div>
+  )
+}
